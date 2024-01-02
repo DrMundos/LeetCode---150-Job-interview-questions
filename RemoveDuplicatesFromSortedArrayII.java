c315492233ccde77ca086f9e1cae8468a6bfb4a4
PureSlayer <101062277+DrMundos@users.noreply.github.com>
2024-01-03 01:10:51 +0200

    Add files via upload

---
+++ RemoveDuplicatesFromSortedArrayII.java
@@ -0,0 +1,22 @@
+//Time complexity - O(n)
+//Space complexity - O(1)
+public class RemoveDuplicatesFromSortedArrayII
+{
+    public int removeDuplicates(int[] nums) 
+    {
+        if(nums.length<3)
+            return nums.length;
+
+        int in=2;
+        for(int i=2;i<nums.length;i++)
+        {
+            if(nums[i]!=nums[in-2])
+            {
+                nums[in]=nums[i];
+                in++;
+            }
+        }
+        return in;
+    }
+
+}
