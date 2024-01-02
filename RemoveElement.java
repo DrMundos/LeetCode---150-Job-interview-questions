db1be971bb9f540967adfdd967cdd52ad50fba64
PureSlayer <101062277+DrMundos@users.noreply.github.com>
2024-01-03 01:06:38 +0200

    Add files via upload

---
+++ RemoveElement.java
@@ -0,0 +1,21 @@
+//Time complexity - O(n)
+//Space complexity - O(1)
+public class RemoveElement 
+{
+    public int removeElement(int[] nums, int val) 
+    {
+        if(nums.length==0)
+            return 0;
+
+        int valid_size=0;
+        for(int i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=val)
+            {
+                nums[valid_size]=nums[i];
+                valid_size++;
+            }
+        }
+        return valid_size;
+    }
+}
